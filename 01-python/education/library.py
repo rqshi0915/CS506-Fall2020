99a1a5d8998aeef5bc86fdbeb8f6643a4450c5f0
Ruoqi Shi <rqshi@bu.edu>
2020-09-14 12:01:49 -0400

    adding a lib

--- 01-python/education/library.py
+++ 01-python/education/library.py
@@ -1,3 +1,26 @@
 def draw_library():
-    print("library not found")
+    print("library is found")
+    library()
     return
+
+import turtle
+import math
+def library() :
+  house=turtle.Turtle()
+  house.pensize(3)
+  house.speed(5)
+  house.color("Blue")
+  side = 150
+  diag = math.sqrt(2*(side**2))
+
+  for i in range(4):
+    house.fd(side)
+    house.lt(90)
+  house.lt(90/2)
+  moves = [(diag,90),(diag/2,90),(diag/2,90),(diag,90)]
+  for (move,turn) in moves:
+    house.fd(move)
+    house.lt(turn)
+
+
+
