b9a16e574e7c747c2037eb69625cda5c5fbe8de0
Ruoqi Shi <rqshi@bu.edu>
2020-09-21 13:00:09 -0400

    02-lib

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,12 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    result = []
+    with open (csv_file_path, "r") as file:
+    	line = file.readlines()
+    	for l in line:
+    		l = l.split(",")
+    		for i in range(len(l)):
+    			l[i] = eval(l[i])
+    		result.append(l)
+    return result
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,37 @@
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    result = 0
+    for i in range(len(x)):
+    	result += abs(x[i] - y[i])**2
+    result = result**(1/2)
+    return result
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    result = 0
+    for i in range(len(x)):
+    	result += abs(x[i] - y[i])
+    return result
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
-
+    result = 0
+    if (x == []):
+    	result = 0
+    elif (y == []):
+    	result = 0
+    else:
+    	result = 1 - ((len(set(x).intersection(set(y))))/len(set(x).union(set(y))))
+    return result
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    result = 0
+    if (x == []):
+    	result = 0
+    elif (y == []):
+    	result = 0
+    elif (euclidean_dist(x,y) == 0):
+    	result = 1
+    else:
+    	for i in range(len(x)):
+    		result += x[i]*y[i]
+    	result = result / euclidean_dist(x,y)
+    return result
 
 # Feel free to add more
